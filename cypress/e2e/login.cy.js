e53bbef3cec305d79513f3f4f849babc0cc8b20d
Pedro Camargo <phenriquecamargo@outlook.com>
2022-10-06 20:50:31 -0300

    Criação do caso de teste da funcionalidade de Login do site Loja EBAC

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+context('Testes da funcionalidade de login',()=>{
+    
+  it('Deve realizar o login corretamente',() =>{
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.get('#username').type("aluno_ebac@teste.com");
+    cy.get('#password').type("teste@teste.com");
+    cy.get('.woocommerce-form > .button').click();
+
+    cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(1)').should('contain','Welcome');
+  })
+
+  it('Deve exibir mensagem de erro ao clicar em realizar login sem inserir usuário e senha',() =>{
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.get('.woocommerce-form > .button').click();
+
+    cy.get('.woocommerce-error > li').should('contain','Erro: Nome de usuário é obrigatório')      
+  })
+
+  it('Deve exibir mensagem de erro ao inserir usuario inválido',() =>{
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.get('#username').type("aluno_ebac@teste.com");
+    cy.get('#password').type("teste@teste"); //Senha Inválida
+    cy.get('.woocommerce-form > .button').click();
+
+    cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail')      
+  })
+
+  it('Deve exibir mensagem de erro ao inserir usuario inválido',() =>{
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.get('#username').type("aluno_ebac@teste"); //Usuário Inválido
+    cy.get('#password').type("teste@teste.com");
+    cy.get('.woocommerce-form > .button').click();
+
+    cy.get('.woocommerce-error > li').should('contain','Erro: O usuário')      
+  })
+
+})
